78dd5388708aa832e0f37f00ab3bf6cc83a35448
MelodyTheMuse <105441849+MelodyTheMuse@users.noreply.github.com>
2024-12-11 22:41:28 +0100

    Got the area to fill in the stream player on the player

--- Addons/musicpacksystem/Scripts/option_button_mood.gd
+++ Addons/musicpacksystem/Scripts/option_button_mood.gd
@@ -7,17 +7,6 @@ func _on_item_selected(index: int) -> void:
 	pass
 	var ASI = "yay"
 	
-func load_dir():
-	var file_path = "res://Addons/musicpacksystem/Themes"
-	var dir = DirAccess.open(file_path)
-	if dir:
-		dir.list_dir_begin()
-		var filename = dir.get_next()
-		while (filename != ""):
-			if filename.ends_with(".tres"):
-				var item = ResourceLoader.load(file_path+"/"+filename)
-				list_themes.append(item)
-				filename = dir.get_next()
+
 func get_mood():
-	load_dir()
-	return list_themes[theme_button.selected].get_clip_stream(selected)
+	return selected
